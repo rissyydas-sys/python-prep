0013473cd607be625d2a247a3ffb0212a74265ef
rissyydas-sys <rissyydas@gmail.com>
2025-09-14 22:02:19 +0530

    Add files via upload
    Message Encoder and Decoder

---
+++ Challenge1.py
@@ -0,0 +1,33 @@
+
+def encode_message(message):
+    encoded_message = ""
+    for index,item in enumerate(message,start=1):
+        index = index%len(item)
+        encoded_message += item[-index:] + item[:-index] + " "
+    return encoded_message
+
+def decode_message(message):
+    decoded_message = ""
+    for index,item in enumerate(message,start=1):
+        index = index%len(item)
+        decoded_message += item[index:] + item[:index] + " "
+    return decoded_message
+
+try:
+    while True:
+        choice = input("1. Encode 2. Decode 3.Exit")
+        if choice == 1:
+            user_input = list(input("Enter the message:").split())
+            print(encode_message(user_input))
+        elif choice == 2:
+            print(decode_message(list(encode_message(user_input).split())))
+        elif choice == 3:
+            break
+        else:
+            print("Invalid choice")
+except:
+    print("Invalid value entered, please enter correctly")
+else:
+    continue
+
+
