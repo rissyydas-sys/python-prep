5f4625ad7537a537a5262554db022c4464885d29
rissyydas-sys <rissyydas@gmail.com>
2025-10-07 17:36:11 +0530

    Update Challenge6.py

--- Challenge6.py
+++ Challenge6.py
@@ -1,16 +1,14 @@
 def number_pattern_generator(num):
     count = 1
     for i in range(1,(num*2)):
-        print(count,end=" ")
-        print()
+        print(count,end="\n")
         if i < num:
             count+=1
         else:
             count-=1
         for j in range(1,count):
-            if j ==num:
-                break
             print(j,end="")
 
 number  = int(input("Enter the number:"))
+
 number_pattern_generator(number)
